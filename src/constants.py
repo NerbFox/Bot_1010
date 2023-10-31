66dda1a889786d66d11a4063a6890f717207e880
unknown <13521043@std.stei.itb.ac.id>
2023-10-31 09:48:21 +0700

    update

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,25 @@
 # success_sound = "../sounds/success-1.mp3"
+
 success_sound = "../sounds/success_bell.mp3"
 start_sound = "../sounds/game-start.mp3"
 game_over_sound = "../sounds/game-over.mp3"
 background_sound = "../sounds/reflected-light.mp3"
+
+# import os
+# import sys
+
+# # Determine the base path based on whether the script is running as an executable
+# if getattr(sys, 'frozen', False):
+#     # Running as a bundled executable
+#     base_path = sys._MEIPASS
+# else:
+#     # Running as a script
+#     base_path = os.path.abspath(os.path.dirname(__file__))
+
+# # Define paths to sound files relative to the base path
+# success_sound = os.path.join(base_path, "sounds", "success_bell.mp3")
+# start_sound = os.path.join(base_path, "sounds", "game-start.mp3")
+# game_over_sound = os.path.join(base_path, "sounds", "game-over.mp3")
+# background_sound = os.path.join(base_path, "sounds", "reflected-light.mp3")
+
+# # Now you can use these paths to load/play the sound files in your code
